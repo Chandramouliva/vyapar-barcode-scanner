1c34809cdb0c912f37e4f12fb440f5cc60a74aeb
unknown <chandramouli@vyaparapp.in>
2021-05-31 15:36:28 +0530

    changed the app.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react"
 import './App.css';
+import Quagga from "quagga"
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+import Barscan from './Components/BarcodeScanner'
 
 function App() {
+  const [startScan, setStartScan] = useState(false)
+
+  const handleScanner = () => {
+    setStartScan(!startScan)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <Router>
+          <div>
+            <div>
+              <button style={{"color": "red", fontSize:"20px"}} onClick = {handleScanner}>{startScan ? "Stop Scan" : "Start Scan" }</button>
+            </div>
+            <div style={{diplsay: "flex", alignSelf:"center"}}>
+              {startScan ? <Barscan/> : <div>Yes its working</div>}
+            </div>
+          </div>
+        </Router>
+      </div>
     </div>
   );
 }
